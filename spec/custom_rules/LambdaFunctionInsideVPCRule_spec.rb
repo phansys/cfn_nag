0eda8250003ae93dc93e6c32fc63511a1d3575b1
Javier Spagnoletti <phansys@gmail.com>
2024-10-19 16:26:21 -0300

    Check W89 with YAML

--- spec/custom_rules/LambdaFunctionInsideVPCRule_spec.rb
+++ spec/custom_rules/LambdaFunctionInsideVPCRule_spec.rb
@@ -9,10 +9,15 @@ describe LambdaFunctionInsideVPCRule do
   describe 'AWS::Lambda::Function' do
     context 'when lambda function is inside VPC' do
       it 'does not return an offending logical resource id' do
-        cfn_model = CfnParser.new.parse read_test_template('json/lambda_function/lambda_inside_vpc.json')
-        actual_logical_resource_ids = LambdaFunctionInsideVPCRule.new.audit_impl cfn_model
+        cfn_model_json = CfnParser.new.parse read_test_template('json/lambda_function/lambda_inside_vpc.json')
+        actual_logical_resource_ids_json = LambdaFunctionInsideVPCRule.new.audit_impl cfn_model_json
 
-        expect(actual_logical_resource_ids).to eq []
+        expect(actual_logical_resource_ids_json).to eq []
+
+        cfn_model_yaml = CfnParser.new.parse read_test_template('yaml/lambda_function/lambda_inside_vpc.yaml')
+        actual_logical_resource_ids_yaml = LambdaFunctionInsideVPCRule.new.audit_impl cfn_model_yaml
+
+        expect(actual_logical_resource_ids_yaml).to eq []
       end
     end
   end
@@ -20,10 +25,15 @@ describe LambdaFunctionInsideVPCRule do
   describe 'AWS::Lambda::Function' do
     context 'when lambda function is not inside VPC' do
       it 'does return an offending logical resource id' do
-        cfn_model = CfnParser.new.parse read_test_template('json/lambda_function/lambda_not_inside_vpc.json')
-        actual_logical_resource_ids = LambdaFunctionInsideVPCRule.new.audit_impl cfn_model
+        cfn_model_json = CfnParser.new.parse read_test_template('json/lambda_function/lambda_not_inside_vpc.json')
+        actual_logical_resource_ids_json = LambdaFunctionInsideVPCRule.new.audit_impl cfn_model_json
 
-        expect(actual_logical_resource_ids).to eq ["FunctionNotInsideVpc"]
+        expect(actual_logical_resource_ids_json).to eq ["FunctionNotInsideVpc"]
+
+        cfn_model_yaml = CfnParser.new.parse read_test_template('yaml/lambda_function/lambda_not_inside_vpc.yaml')
+        actual_logical_resource_ids_yaml = LambdaFunctionInsideVPCRule.new.audit_impl cfn_model_yaml
+
+        expect(actual_logical_resource_ids_yaml).to eq ["FunctionNotInsideVpc"]
       end
     end
    end
